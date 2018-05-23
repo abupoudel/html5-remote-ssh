0a14db01c4b35095375a59ff41d8823a1e440e2e
abu <admin@techjail.net>
2018-05-23 23:33:17 +0545

    Project initialized

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# html5-remote-ssh
+# HTML5 Remote SSH
 HTML5 based Remote SSH with Python backend
+
+# Table of Content
+  * Requirements
+  * Python setup
+  * WebsocketServer
+  * HTML Setup
+
+# Requirements
+  - Python 2.7 (Tested), may work with other version of Python
+  - HTML5 browser supporting WebSocket and JSON(you can replace JSON with other library if you donot have support of it)
+
+# Python setup
+## Paramiko
+`Paramiko<https://github.com/paramiko/paramiko/>` is a python based SSH client with good documentation. You can easily install it using following command
+```
+pip install paramiko
+```
+
+## WebsocketServer
+I wanted a simplest of the WebSocket server so I selected `this<https://github.com/Pithikos/python-websocket-server>`. As state `here<https://github.com/Pithikos/python-websocket-server#installation>` about the installation, you can easily install it by:
+  1. `pip install git+https://github.com/Pithikos/python-websocket-server` (latest code)
+  2. `pip install websocket-server` (might not be up-to-date)
+
+# HTML Setup
+You can just copy [shell.html](html/shell.html) and edit few things up depending on your requirements. The main connection line looks like below and can be edited to set the host/username/password you required
+```
+mainSocket.send(JSON.stringify({action: 'connect', host:"localhost",port:22,username:'user',password:'password'}));
+```
+## Parameters
+  - action:		'connect' (default to connect for connection)
+  - host:		the host you would like to connect to. localhost, www.google.com, 192.168.1.5(this will connect to the server's local network and not your own)
+  - port:		default is 22, you can send the port you want
+  - username:	the username you want to connect to
+  - password:	the password you connect to server for the username
---
+++ python/shel.py
@@ -0,0 +1,85 @@
+# encoding=utf8 
+import paramiko									# To handle the SSH
+import time										# Handling some sleep
+import json										# We are relying on JSON object to send/recieve between browser and socket
+from thread import start_new_thread				# Handling multiple SSH sessions
+from websocket_server import WebsocketServer	# Minimal WebSocket library
+
+# Client lists to handle connections
+clients = {}
+
+# We new client connects
+def new_client(client, server):
+	print("New client connected")
+	clients[client["id"]] = {
+		'client' : client,
+		'in_active_connection' : False,
+		'close_me' : False,
+		'closed' : False,
+		'terminal' : paramiko.SSHClient(),
+		'channel' : None
+	}
+	myClient = clients[client["id"]]['terminal']
+	myClient.set_missing_host_key_policy(paramiko.WarningPolicy())
+
+# starting the connection to new SSH Connection
+def start_new_shell(client,server, host, port, username, password):
+	myClient = clients[client["id"]]['terminal']
+	try:
+		myClient.connect(host, username=username, password=password, port=port)
+		clients[client["id"]]['in_active_connection'] = True
+		clients[client["id"]]['closed'] = False
+		server.send_message(client,json.dumps({'action':'connection','success':True}))
+	except:
+		clients[client["id"]]['closed'] = True
+		server.send_message(client,json.dumps({'action':'connection','success':False}))
+		#bye_client(client,server)
+		return
+	
+	clients[client["id"]]['channel'] = myClient.invoke_shell()
+	chan = clients[client["id"]]['channel'] 
+	while True:
+		if clients[client["id"]]['close_me']:
+			break
+		if chan.recv_ready():
+			server.send_message(client,json.dumps({'action':'message','message':unicode(chan.recv(1024))}))
+		time.sleep(0.01)
+	chan.close()
+	myClient.close()
+	clients[client["id"]]['closed'] = True
+
+# When WebSocket client closes
+def bye_client(client, server):
+	print "Bye," , client
+	clients[client['id']]["close_me"] = True
+	while not clients[client['id']]["closed"]:
+		time.sleep(0.01)
+	client["handler"].connection.close()
+	del clients[client['id']]
+	print len(clients)
+
+# When we recieve thing from WebSocket browser
+def message_client(client,server,message):
+	json_d = json.loads(message)
+	try:
+		if json_d["action"] == "connect":
+			start_new_thread(start_new_shell,(client,server,json_d["host"],json_d["port"],json_d["username"],json_d["password"],))
+		elif json_d["action"] == "key":
+			chan = clients[client["id"]]['channel']
+			chan.send(json_d["key"])
+		elif json_d["action"] == "keySpecial":
+			chan = clients[client["id"]]['channel']
+			json_d["key"] = "\x1B" + json_d["key"]
+			chan.send(json_d["key"])
+		elif json_d["action"] == "keyCode":
+			chan = clients[client["id"]]['channel']
+			chan.send(chr(json_d["keyCode"]))
+	except:
+		bye_client(client, server)
+
+# Listening to our desired port and setting up the connection
+server = WebsocketServer(13254, host='0.0.0.0')
+server.set_fn_new_client(new_client)
+server.set_fn_client_left(bye_client)
+server.set_fn_message_received(message_client)
+server.run_forever()
